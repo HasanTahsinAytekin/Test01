fd5276fcf91fc3ac8c2225f1f46247cfeca5473f
Hasan Aytekin <aytekin.hasan.tahsin@gmail.com>
2023-01-15 14:28:25 +0300

    Test #1

---
+++ Matrix/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Matrix.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    Matrix::Matrix* x = new Matrix::Matrix(1,1);
+
+
+    return 0;
+
+
+}
